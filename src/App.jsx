c5ae4bdc3c619921853603d9c24256657e4ae309
Vyshnav M <vyshnavvijayan99@gmail.com>
2025-04-17 10:03:28 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
 import Layout from './connection/Layout'
 import Dashboard from './pages/private/Dashboard'
 import { Route, Routes } from 'react-router-dom'
