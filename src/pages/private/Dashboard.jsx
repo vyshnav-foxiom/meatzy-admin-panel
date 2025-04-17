9308429f5d045e9dc1d33c5158dafef5d2f2a1b4
Vyshnav M <vyshnavvijayan99@gmail.com>
2025-04-17 11:47:06 +0530

    .

--- src/pages/private/Dashboard.jsx
+++ src/pages/private/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import bg from "@/assets/images/wall-fish.jpg"
 const Dashboard = () => {
   return (
-    <div className='bg-amber-300'>Dashboard</div>
+    <div
+      className="relative h-screen w-full bg-cover bg-center"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="absolute inset-0 bg-black/50" /> {/* overlay */}
+      <div className="relative z-10 text-white p-8">Dashboard</div>
+    </div>
+
   )
 }
 
